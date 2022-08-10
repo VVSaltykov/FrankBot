5f9cb0f15bdd52b1358db67bd878a5d79f06de0f
VVSaltykov <saltyckov.vitya@yandex.ru>
2022-08-10 21:53:00 +0400

    Add project files.

---
+++ FrankBot/Program.cs
@@ -0,0 +1,27 @@
+﻿using Telegram.Bot;
+using Telegram.Bot.Types.Enums;
+using Telegram.Bot.Args;
+
+namespace FrankBot
+{
+    internal class Program
+    {
+        private static string Token { get; set; } = "";//TODO - insert correct token
+        private static TelegramBotClient client;
+        [Obsolete]
+        static void Main()
+        {
+
+        }
+        [Obsolete]
+        private static async void OnCallbackQweryHandlerAsync(object sender, CallbackQueryEventArgs e)
+        {
+
+        }
+        [Obsolete]
+        private static async void OnMessageHandler(object sender, MessageEventArgs e)
+        {
+
+        }
+    }
+}
