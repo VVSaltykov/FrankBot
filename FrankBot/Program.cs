facc1f7655625c5ea245491732d3e448157d987d
VVSaltykov <saltyckov.vitya@yandex.ru>
2022-08-16 18:33:06 +0400

    Add current output

--- FrankBot/Program.cs
+++ FrankBot/Program.cs
@@ -48,6 +48,134 @@ namespace FrankBot
             {
                 await client.SendTextMessageAsync(message.Chat.Id, Messages.EUR, replyMarkup: Buttons.Start());
             }
+            else if (e.CallbackQuery.Data == "AUD")
+            {
+                await client.SendTextMessageAsync(message.Chat.Id, Messages.AUD, replyMarkup: Buttons.Start());
+            }
+            else if (e.CallbackQuery.Data == "AZN")
+            {
+                await client.SendTextMessageAsync(message.Chat.Id, Messages.AZN, replyMarkup: Buttons.Start());
+            }
+            else if (e.CallbackQuery.Data == "GBP")
+            {
+                await client.SendTextMessageAsync(message.Chat.Id, Messages.GBP, replyMarkup: Buttons.Start());
+            }
+            else if (e.CallbackQuery.Data == "AMD")
+            {
+                await client.SendTextMessageAsync(message.Chat.Id, Messages.AMD, replyMarkup: Buttons.Start());
+            }
+            else if (e.CallbackQuery.Data == "BYN")
+            {
+                await client.SendTextMessageAsync(message.Chat.Id, Messages.BYN, replyMarkup: Buttons.Start());
+            }
+            else if (e.CallbackQuery.Data == "BGN")
+            {
+                await client.SendTextMessageAsync(message.Chat.Id, Messages.BGN, replyMarkup: Buttons.Start());
+            }
+            else if (e.CallbackQuery.Data == "BRL")
+            {
+                await client.SendTextMessageAsync(message.Chat.Id, Messages.BRL, replyMarkup: Buttons.Start());
+            }
+            else if (e.CallbackQuery.Data == "HUF")
+            {
+                await client.SendTextMessageAsync(message.Chat.Id, Messages.HUF, replyMarkup: Buttons.Start());
+            }
+            else if (e.CallbackQuery.Data == "HKD")
+            {
+                await client.SendTextMessageAsync(message.Chat.Id, Messages.HKD, replyMarkup: Buttons.Start());
+            }
+            else if (e.CallbackQuery.Data == "DKK")
+            {
+                await client.SendTextMessageAsync(message.Chat.Id, Messages.DKK, replyMarkup: Buttons.Start());
+            }
+            else if (e.CallbackQuery.Data == "INR")
+            {
+                await client.SendTextMessageAsync(message.Chat.Id, Messages.INR, replyMarkup: Buttons.Start());
+            }
+            else if (e.CallbackQuery.Data == "KZT")
+            {
+                await client.SendTextMessageAsync(message.Chat.Id, Messages.KZT, replyMarkup: Buttons.Start());
+            }
+            else if (e.CallbackQuery.Data == "CAD")
+            {
+                await client.SendTextMessageAsync(message.Chat.Id, Messages.CAD, replyMarkup: Buttons.Start());
+            }
+            else if (e.CallbackQuery.Data == "KGS")
+            {
+                await client.SendTextMessageAsync(message.Chat.Id, Messages.KGS, replyMarkup: Buttons.Start());
+            }
+            else if (e.CallbackQuery.Data == "CNY")
+            {
+                await client.SendTextMessageAsync(message.Chat.Id, Messages.CNY, replyMarkup: Buttons.Start());
+            }
+            else if (e.CallbackQuery.Data == "MDL")
+            {
+                await client.SendTextMessageAsync(message.Chat.Id, Messages.MDL, replyMarkup: Buttons.Start());
+            }
+            else if (e.CallbackQuery.Data == "NOK")
+            {
+                await client.SendTextMessageAsync(message.Chat.Id, Messages.NOK, replyMarkup: Buttons.Start());
+            }
+            else if (e.CallbackQuery.Data == "PLN")
+            {
+                await client.SendTextMessageAsync(message.Chat.Id, Messages.PLN, replyMarkup: Buttons.Start());
+            }
+            else if (e.CallbackQuery.Data == "RON")
+            {
+                await client.SendTextMessageAsync(message.Chat.Id, Messages.RON, replyMarkup: Buttons.Start());
+            }
+            else if (e.CallbackQuery.Data == "XDR")
+            {
+                await client.SendTextMessageAsync(message.Chat.Id, Messages.XDR, replyMarkup: Buttons.Start());
+            }
+            else if (e.CallbackQuery.Data == "SGD")
+            {
+                await client.SendTextMessageAsync(message.Chat.Id, Messages.SGD, replyMarkup: Buttons.Start());
+            }
+            else if (e.CallbackQuery.Data == "TJS")
+            {
+                await client.SendTextMessageAsync(message.Chat.Id, Messages.TJS, replyMarkup: Buttons.Start());
+            }
+            else if (e.CallbackQuery.Data == "TRY")
+            {
+                await client.SendTextMessageAsync(message.Chat.Id, Messages.TRY, replyMarkup: Buttons.Start());
+            }
+            else if (e.CallbackQuery.Data == "TMT")
+            {
+                await client.SendTextMessageAsync(message.Chat.Id, Messages.TMT, replyMarkup: Buttons.Start());
+            }
+            else if (e.CallbackQuery.Data == "UZS")
+            {
+                await client.SendTextMessageAsync(message.Chat.Id, Messages.UZS, replyMarkup: Buttons.Start());
+            }
+            else if (e.CallbackQuery.Data == "UAH")
+            {
+                await client.SendTextMessageAsync(message.Chat.Id, Messages.UAH, replyMarkup: Buttons.Start());
+            }
+            else if (e.CallbackQuery.Data == "CZK")
+            {
+                await client.SendTextMessageAsync(message.Chat.Id, Messages.CZK, replyMarkup: Buttons.Start());
+            }
+            else if (e.CallbackQuery.Data == "SEK")
+            {
+                await client.SendTextMessageAsync(message.Chat.Id, Messages.SEK, replyMarkup: Buttons.Start());
+            }
+            else if (e.CallbackQuery.Data == "CHF")
+            {
+                await client.SendTextMessageAsync(message.Chat.Id, Messages.CHF, replyMarkup: Buttons.Start());
+            }
+            else if (e.CallbackQuery.Data == "ZAR")
+            {
+                await client.SendTextMessageAsync(message.Chat.Id, Messages.ZAR, replyMarkup: Buttons.Start());
+            }
+            else if (e.CallbackQuery.Data == "KRW")
+            {
+                await client.SendTextMessageAsync(message.Chat.Id, Messages.KRW, replyMarkup: Buttons.Start());
+            }
+            else if (e.CallbackQuery.Data == "JPY")
+            {
+                await client.SendTextMessageAsync(message.Chat.Id, Messages.JPY, replyMarkup: Buttons.Start());
+            }
         }
         [Obsolete]
         private static async void OnMessageHandler(object sender, MessageEventArgs e)
--- FrankBot/UI/Messages.cs
+++ FrankBot/UI/Messages.cs
@@ -13,5 +13,37 @@ namespace FrankBot.UI
         public static string CurrencyMenu = "Select the currency you want to know the exchange rate of";
         public static string USD = $"At the moment, the USD to RUB exchange rate is as follows: {CurrentRepositore.USDReader()}";
         public static string EUR = $"At the moment, the EUR to RUB exchange rate is as follows: {CurrentRepositore.EURReader()}";
+        public static string AUD = $"At the moment, the AUD to RUB exchange rate is as follows: {CurrentRepositore.AUDReader()}";
+        public static string AZN = $"At the moment, the AZN to RUB exchange rate is as follows: {CurrentRepositore.AZNReader()}";
+        public static string GBP = $"At the moment, the GBP to RUB exchange rate is as follows: {CurrentRepositore.GBPReader()}";
+        public static string AMD = $"At the moment, the AMD to RUB exchange rate is as follows: {CurrentRepositore.AMDReader()}";
+        public static string BYN = $"At the moment, the BYN to RUB exchange rate is as follows: {CurrentRepositore.BYNReader()}";
+        public static string BGN = $"At the moment, the BGN to RUB exchange rate is as follows: {CurrentRepositore.BGNReader()}";
+        public static string BRL = $"At the moment, the BRL to RUB exchange rate is as follows: {CurrentRepositore.BRLReader()}";
+        public static string HUF = $"At the moment, the HUF to RUB exchange rate is as follows: {CurrentRepositore.HUFReader()}";
+        public static string HKD = $"At the moment, the HKD to RUB exchange rate is as follows: {CurrentRepositore.HKDReader()}";
+        public static string DKK = $"At the moment, the DKK to RUB exchange rate is as follows: {CurrentRepositore.DKKReader()}";
+        public static string INR = $"At the moment, the INR to RUB exchange rate is as follows: {CurrentRepositore.INRReader()}";
+        public static string KZT = $"At the moment, the KZT to RUB exchange rate is as follows: {CurrentRepositore.KZTReader()}";
+        public static string CAD = $"At the moment, the CAD to RUB exchange rate is as follows: {CurrentRepositore.CADReader()}";
+        public static string KGS = $"At the moment, the KGS to RUB exchange rate is as follows: {CurrentRepositore.KGSReader()}";
+        public static string CNY = $"At the moment, the CNY to RUB exchange rate is as follows: {CurrentRepositore.CNYReader()}";
+        public static string MDL = $"At the moment, the MDL to RUB exchange rate is as follows: {CurrentRepositore.MDLReader()}";
+        public static string NOK = $"At the moment, the NOK to RUB exchange rate is as follows: {CurrentRepositore.NOKReader()}";
+        public static string PLN = $"At the moment, the PLN to RUB exchange rate is as follows: {CurrentRepositore.PLNReader()}";
+        public static string RON = $"At the moment, the RON to RUB exchange rate is as follows: {CurrentRepositore.RONReader()}";
+        public static string XDR = $"At the moment, the XDR to RUB exchange rate is as follows: {CurrentRepositore.XDRReader()}";
+        public static string SGD = $"At the moment, the SGD to RUB exchange rate is as follows: {CurrentRepositore.SGDReader()}";
+        public static string TJS = $"At the moment, the TJS to RUB exchange rate is as follows: {CurrentRepositore.TJSReader()}";
+        public static string TRY = $"At the moment, the TRY to RUB exchange rate is as follows: {CurrentRepositore.TRYReader()}";
+        public static string TMT = $"At the moment, the TMT to RUB exchange rate is as follows: {CurrentRepositore.TMTReader()}";
+        public static string UZS = $"At the moment, the UZS to RUB exchange rate is as follows: {CurrentRepositore.UZSReader()}";
+        public static string UAH = $"At the moment, the UAH to RUB exchange rate is as follows: {CurrentRepositore.UAHReader()}";
+        public static string CZK = $"At the moment, the CZK to RUB exchange rate is as follows: {CurrentRepositore.CZKReader()}";
+        public static string SEK = $"At the moment, the SEK to RUB exchange rate is as follows: {CurrentRepositore.SEKReader()}";
+        public static string CHF = $"At the moment, the CHF to RUB exchange rate is as follows: {CurrentRepositore.CHFReader()}";
+        public static string ZAR = $"At the moment, the ZAR to RUB exchange rate is as follows: {CurrentRepositore.ZARReader()}";
+        public static string KRW = $"At the moment, the KRW to RUB exchange rate is as follows: {CurrentRepositore.KRWReader()}";
+        public static string JPY = $"At the moment, the JPY to RUB exchange rate is as follows: {CurrentRepositore.JPYReader()}";
     }
 }
